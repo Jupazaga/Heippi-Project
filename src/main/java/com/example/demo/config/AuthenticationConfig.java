1a27df475cf3344fde6ab0fa5acc5cb5cdd1d033
Pablo Z <juan.zapata046@pascualbravo.edu.co>
2024-09-22 11:44:49 -0500

    fix: initial configuration for spring security

---
+++ src/main/java/com/example/demo/config/AuthenticationConfig.java
@@ -0,0 +1,31 @@
+package com.example.demo.config;
+
+import com.example.demo.domain.Usuario;
+import com.example.demo.repository.UsuariosRepository;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+public class AuthenticationConfig implements UserDetailsService {
+    private final UsuariosRepository usuariosRepository;
+    public AuthenticationConfig(UsuariosRepository usuariosRepository) {
+        this.usuariosRepository = usuariosRepository;
+    }
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Usuario usuario = usuariosRepository.findUsuarioByIdentificacion(username);
+        List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority(usuario.getAuthority().toString()));
+        return new User(usuario.getIdentificacion(), usuario.getPassword(), authorities);
+    }
+}
--- src/main/java/com/example/demo/service/UsuarioService.java
+++ src/main/java/com/example/demo/service/UsuarioService.java
@@ -3,18 +3,21 @@ package com.example.demo.service;
 import com.example.demo.domain.Usuario;
 import com.example.demo.repository.UsuariosRepository;
 import jakarta.transaction.Transactional;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 
 @Service
 @Transactional
 public class UsuarioService {
     private final UsuariosRepository usuariosRepository;
-    public UsuarioService(UsuariosRepository usuariosRepository) {
+    private final PasswordEncoder passwordEncoder;
+    public UsuarioService(UsuariosRepository usuariosRepository, PasswordEncoder passwordEncoder) {
         this.usuariosRepository = usuariosRepository;
+        this.passwordEncoder = passwordEncoder;
     }
     public void crearUsuario(Usuario usuario) {
+        usuario.setPassword(passwordEncoder.encode(usuario.getPassword()));
         usuariosRepository.save(usuario);
     }
 
