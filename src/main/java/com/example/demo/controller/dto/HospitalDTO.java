030b2796465e3297adb347223100f72d384f975e
Pablo Z <juan.zapata046@pascualbravo.edu.co>
2024-10-11 18:49:55 -0500

    fix: optimize imports

--- src/main/java/com/example/demo/controller/dto/HospitalDTO.java
+++ src/main/java/com/example/demo/controller/dto/HospitalDTO.java
@@ -2,9 +2,11 @@ package com.example.demo.controller.dto;
 
 import lombok.Data;
 
+import java.io.Serial;
 import java.io.Serializable;
 @Data
 public class HospitalDTO implements Serializable {
+    @Serial
     private static final long serialVersionUID = 1L;
     private String id;
     private String nombre;
--- src/main/java/com/example/demo/domain/Hospital.java
+++ src/main/java/com/example/demo/domain/Hospital.java
@@ -3,7 +3,6 @@ package com.example.demo.domain;
 import jakarta.persistence.*;
 import lombok.Data;
 
-import java.io.Serializable;
 import java.util.List;
 
 @Data
