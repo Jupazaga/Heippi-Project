261329373c3e2e1be6f62647751232231dbfcce9
Pablo Z <juan.zapata046@pascualbravo.edu.co>
2024-10-11 12:02:41 -0500

    feature: Service to send a simple message on email

---
+++ src/main/java/com/example/demo/service/EmailService.java
@@ -0,0 +1,28 @@
+package com.example.demo.service;
+
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailService {
+    private final JavaMailSender mailSender;
+
+    public EmailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void sendSimpleMessage(String from, String to, String subject, String text) {
+        try{
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom(from);
+            message.setTo(to);
+            message.setSubject(subject);
+            message.setText(text);
+            mailSender.send(message);
+        }catch (Exception e){
+            System.out.println("Could not sent email");
+            System.out.println(e);
+        }
+    }
+}
