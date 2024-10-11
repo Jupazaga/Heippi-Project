e1fd7013f399854de0dbf6a58ea727d6764fd1d1
Pablo Z <juan.zapata046@pascualbravo.edu.co>
2024-10-11 11:56:25 -0500

    feature: Methods to request and update password

--- src/main/java/com/example/demo/controller/UsuarioController.java
+++ src/main/java/com/example/demo/controller/UsuarioController.java
@@ -1,11 +1,11 @@
 package com.example.demo.controller;
 
+import com.example.demo.domain.Recovery;
 import com.example.demo.domain.Usuario;
 import com.example.demo.service.UsuarioService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("/usuarios")
@@ -23,5 +23,20 @@ public class UsuarioController {
 
         usuarioService.crearUsuario(usuario);
         return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/password")
+    public ResponseEntity<Void> requestRecovery(@RequestBody Usuario usuario) {
+        String email = usuario.getEmail();
+        usuarioService.requestPasswordReset(email);
+        return ResponseEntity.accepted().build();
+    }
+
+    @PatchMapping("/password")
+    public ResponseEntity<Void> updatePassword(@RequestParam String token, @RequestParam String password) {
+        Recovery recovery = new Recovery();
+        recovery.setToken(token);
+        usuarioService.resetPassword(recovery, password);
+        return ResponseEntity.noContent().build();
     }
 }
