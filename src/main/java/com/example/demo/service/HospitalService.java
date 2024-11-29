93ad486276501cd4c633b1777fe28cc287785926
Pablo Z <juan.zapata046@pascualbravo.edu.co>
2024-11-29 16:58:30 +0000

    fix: When creating a Hospital, search an Usuario with the same id and attached to hospital due to one-to-one relationship

--- src/main/java/com/example/demo/service/HospitalService.java
+++ src/main/java/com/example/demo/service/HospitalService.java
@@ -2,7 +2,10 @@ package com.example.demo.service;
 
 import com.example.demo.controller.dto.HospitalDTO;
 import com.example.demo.controller.mapper.HospitalMapper;
+import com.example.demo.domain.Hospital;
+import com.example.demo.domain.Usuario;
 import com.example.demo.repository.HospitalRepository;
+import com.example.demo.repository.UsuariosRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
@@ -13,13 +16,19 @@ import java.util.List;
 public class HospitalService {
     private final HospitalMapper hospitalMapper;
     private final HospitalRepository hospitalRepository;
-    public HospitalService(HospitalRepository hospitalRepository) {
+    private final UsuariosRepository usuariosRepository;
+
+    public HospitalService(HospitalRepository hospitalRepository, UsuariosRepository usuariosRepository) {
         this.hospitalRepository = hospitalRepository;
         this.hospitalMapper = new HospitalMapper();
+        this.usuariosRepository = usuariosRepository;
     }
 
     public void createHospital(HospitalDTO hospitalDTO) {
-        hospitalRepository.save(hospitalMapper.hospitalDTOtoHospital(hospitalDTO));
+        Hospital hospital = hospitalMapper.hospitalDTOtoHospital(hospitalDTO);
+        Usuario usuario = usuariosRepository.findUsuarioByIdentificacion(hospital.getId());
+        hospital.setUsuario(usuario);
+        hospitalRepository.save(hospital);
     }
 
     public List<HospitalDTO> getListHospitals() {
