c77d34b12b1955d3fd96d459e97be372fc2469d6
Pablo Z <juan.zapata046@pascualbravo.edu.co>
2024-11-30 00:43:37 +0000

    fix: Usuario is added to Medico with newPassword for one-to-one relationship

--- src/main/java/com/example/demo/service/MedicoService.java
+++ src/main/java/com/example/demo/service/MedicoService.java
@@ -2,7 +2,11 @@ package com.example.demo.service;
 
 import com.example.demo.controller.dto.MedicoDTO;
 import com.example.demo.controller.mapper.MedicoMapper;
+import com.example.demo.domain.Medico;
+import com.example.demo.domain.Usuario;
 import com.example.demo.repository.MedicoRepository;
+import com.example.demo.repository.UsuariosRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,13 +17,21 @@ import java.util.List;
 public class MedicoService {
     private final MedicoRepository medicoRepository;
     private final MedicoMapper medicoMapper;
+    private final UsuariosRepository usuariosRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public MedicoService(MedicoRepository medicoRepository, MedicoMapper medicoMapper) {
+    public MedicoService(MedicoRepository medicoRepository, MedicoMapper medicoMapper, UsuariosRepository usuariosRepository, PasswordEncoder passwordEncoder) {
         this.medicoRepository = medicoRepository;
         this.medicoMapper = medicoMapper;
+        this.usuariosRepository = usuariosRepository;
+        this.passwordEncoder = passwordEncoder;
     }
     public void create(MedicoDTO medicoDTO) {
-        medicoRepository.save(medicoMapper.medicoDTOToMedico(medicoDTO));
+        Medico medico = medicoMapper.medicoDTOToMedico(medicoDTO);
+        Usuario usuario = usuariosRepository.findUsuarioByIdentificacion(medicoDTO.getId());
+        usuario.setPassword(passwordEncoder.encode(medicoDTO.getNewPassword()));
+        medico.setUsuario(usuario);
+        medicoRepository.save(medico);
     }
 
     public List<MedicoDTO> getAllMedicos() {
