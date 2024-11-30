bb3aeda133d21d827982ea48b2261ff90c298d48
Pablo Z <juan.zapata046@pascualbravo.edu.co>
2024-11-30 00:46:00 +0000

    feature: Usuario added to Paciente for one-to-one relationship

--- src/main/java/com/example/demo/service/PacienteService.java
+++ src/main/java/com/example/demo/service/PacienteService.java
@@ -2,7 +2,9 @@ package com.example.demo.service;
 
 import com.example.demo.controller.dto.PacienteDTO;
 import com.example.demo.controller.mapper.PacienteMapper;
+import com.example.demo.domain.Paciente;
 import com.example.demo.repository.PacienteRepository;
+import com.example.demo.repository.UsuariosRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
@@ -13,13 +15,17 @@ import java.util.List;
 public class PacienteService {
     private final PacienteRepository pacienteRepository;
     private final PacienteMapper pacienteMapper;
+    private final UsuariosRepository usuariosRepository;
 
-    public PacienteService(PacienteRepository pacienteRepository, PacienteMapper pacienteMapper) {
+    public PacienteService(PacienteRepository pacienteRepository, PacienteMapper pacienteMapper, UsuariosRepository usuariosRepository) {
         this.pacienteRepository = pacienteRepository;
         this.pacienteMapper = pacienteMapper;
+        this.usuariosRepository = usuariosRepository;
     }
     public void crearPaciente(PacienteDTO pacienteDTO) {
-        pacienteRepository.save(pacienteMapper.pacienteDTOToPaciente(pacienteDTO));
+        Paciente paciente = pacienteMapper.pacienteDTOToPaciente(pacienteDTO);
+        paciente.setUsuario(usuariosRepository.findUsuarioByIdentificacion(paciente.getId()));
+        pacienteRepository.save(paciente);
     }
 
     public List<PacienteDTO> getAllPacientes() {
