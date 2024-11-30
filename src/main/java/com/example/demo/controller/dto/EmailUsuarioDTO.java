e62984c0f6733f5610d436d459b53ea21c6df9c8
Pablo Z <juan.zapata046@pascualbravo.edu.co>
2024-11-30 01:24:10 +0000

    refactor: Added Serial annotation for EmailUsuarioDTO version

--- src/main/java/com/example/demo/controller/dto/EmailUsuarioDTO.java
+++ src/main/java/com/example/demo/controller/dto/EmailUsuarioDTO.java
@@ -4,10 +4,12 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
+import java.io.Serial;
 import java.io.Serializable;
 
 @Data
 public class EmailUsuarioDTO implements Serializable {
+    @Serial
     private static final long serialVersionUID = 1L;
     @Email
     @NotBlank(message = "Email is mandatory")
